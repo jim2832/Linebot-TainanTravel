ad9ca0e9f464c99ee7f770d14c98542a2deadf47
Jim Lee <jim880705@gmail.com>
2021-07-07 15:51:02 +0800

    ok

--- text_reply.py
+++ text_reply.py
@@ -22,3 +22,10 @@ def text_reply_message(user_message):
         output_message = user_message  
 
     return TextSendMessage(text=output_message)
+
+def image_reply_message(user_message):
+    if(user_message == "可樂"):
+        output_message = ImageSendMessage(
+            original_content_url = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.xuite.net%2Fchungming01%2Ftwblog%2F106578814&psig=AOvVaw0JV7TR5GNy4349MN-bKgF0&ust=1625729867993000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCIDOjtK80PECFQAAAAAdAAAAABAD"
+            preview_image_url = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.xuite.net%2Fchungming01%2Ftwblog%2F106578814&psig=AOvVaw0JV7TR5GNy4349MN-bKgF0&ust=1625729867993000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCIDOjtK80PECFQAAAAAdAAAAABAD"
+        )

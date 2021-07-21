804e076198dabd1a22ec9f787633d5bfe24e4bde
Jim Lee <jim880705@gmail.com>
2021-07-21 23:14:51 +0800

    ok

--- start.py
+++ start.py
@@ -154,7 +154,7 @@ def handle_message(event):
 
         elif user_message == "台南旅遊" or "台南" or "旅遊":
             output_message = ImagemapSendMessage(
-                base_url = "https://i.imgur.com/mWTRRGz.jpg",
+                base_url = "https://i.imgur.com/mWTRRGz.jpg#",
                 alt_text = "此裝置不支援樣板。", #無法支援格式所顯示的文字
                 base_size = Basesize(height = 1500, width = 1500),
                 actions =[

8831ba69fde6a5dad8fd0720fe82e2703980c7ad
Jim Lee <jim880705@gmail.com>
2021-07-21 21:10:57 +0800

    ok

--- start.py
+++ start.py
@@ -170,7 +170,6 @@ def handle_message(event):
                 latitude = "22.99762337852117",
                 longitude = "120.2024704919533"
             )
-
             line_bot_api.reply_message(event.reply_token, output_message)
 
         #2
@@ -181,7 +180,6 @@ def handle_message(event):
                 latitude = "23.001593229535548",
                 longitude = "120.1606351263452"
             )
-
             line_bot_api.reply_message(event.reply_token, output_message)
 
         #3
@@ -192,7 +190,6 @@ def handle_message(event):
                 latitude = "22.93480286259137",
                 longitude = "120.2260482551798"
             )
-
             line_bot_api.reply_message(event.reply_token, output_message)
         
         #4
@@ -203,7 +200,6 @@ def handle_message(event):
                 latitude = "22.99753585895218",
                 longitude = "120.19648398291852"
             )
-
             line_bot_api.reply_message(event.reply_token, output_message)
 
         #5
@@ -214,7 +210,6 @@ def handle_message(event):
                 latitude = "22.98054329215947",
                 longitude = "120.15580504320876"
             )            
-
             line_bot_api.reply_message(event.reply_token, output_message)
 
         #6
@@ -225,7 +220,6 @@ def handle_message(event):
                 latitude = "23.003306864536732",
                 longitude = "120.15982008130227"
             )            
-
             line_bot_api.reply_message(event.reply_token, output_message)
 
         #7
@@ -236,7 +230,6 @@ def handle_message(event):
                 latitude = "23.01159041194204",
                 longitude = "120.20039541306427"
             )            
-
             line_bot_api.reply_message(event.reply_token, output_message)
 
         #8
@@ -247,7 +240,6 @@ def handle_message(event):
                 latitude = "23.002783289337064",
                 longitude = "120.15633702634503"
             )            
-
             line_bot_api.reply_message(event.reply_token, output_message)
 
         #9
@@ -258,7 +250,6 @@ def handle_message(event):
                 latitude = "22.990712678956807",
                 longitude = "120.20431901470467"
             )            
-
             line_bot_api.reply_message(event.reply_token, output_message)
 
         #10
@@ -269,7 +260,6 @@ def handle_message(event):
                 latitude = "23.154298515853103",
                 longitude = "120.09994515333189"
             )            
-
             line_bot_api.reply_message(event.reply_token, output_message)
 
 
@@ -601,6 +591,205 @@ def handle_message(event):
             #-------------------------------------------------------------
 
             #地圖位置
+        #1
+        elif user_message ==f"可由此打開{food1}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food1}",
+                address = "台南市中西區國華街三段5號",
+                latitude = "22.993674901918126",
+                longitude = "120.19748719741693"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #2
+        elif user_message ==f"可由此打開{food2}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food2}",
+                address = "台南市中西區海安路一段66號",
+                latitude = "22.9891276361631",
+                longitude = "120.19522919754588"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #3
+        elif user_message ==f"可由此打開{food3}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food3}",
+                address = "台南市中西區國華街三段181號",
+                latitude = "22.997732178963766",
+                longitude = "120.19892455521706"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #4
+        elif user_message ==f"可由此打開{food4}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food4}",
+                address = "708台南市安平區安平路590號",
+                latitude = "22.998952853479697",
+                longitude = "120.16973116871044"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #5
+        elif user_message ==f"可由此打開{food5}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food5}",
+                address = "台南市中西區保安路53號",
+                latitude = "22.990440941349757",
+                longitude = "120.19641742638149"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #6
+        elif user_message ==f"可由此打開{food6}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food6}",
+                address = "台南市東區勝利路119號",
+                latitude = "22.994935069427772",
+                longitude = "120.2179041398749"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #7
+        elif user_message ==f"可由此打開{food7}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food7}",
+                address = "台南市中西區中山路8巷5號",
+                latitude = "22.99301999070557",
+                longitude = "120.20598721288803"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #8
+        elif user_message ==f"可由此打開{food8}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food8}",
+                address = "台南市安平區安平路612號",
+                latitude = "22.999127249166314",
+                longitude = "120.1689278840525"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #9
+        elif user_message ==f"可由此打開{food9}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food9}",
+                address = "台南市中西區民族路三段89號",
+                latitude = "22.998513225860354",
+                longitude = "120.19694906871052"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #10
+        elif user_message ==f"可由此打開{food10}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food10}",
+                address = "台南市安平區安平路408-1號",
+                latitude = "22.998325034720985",
+                longitude = "120.17461905521701"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #11
+        elif user_message ==f"可由此打開{food11}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food11}",
+                address = "台南市中西區友愛街143號",
+                latitude = "22.991698263035705",
+                longitude = "120.19915861288803"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #12
+        elif user_message ==f"可由此打開{food12}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food12}",
+                address = "台南市中西區民族路三段11號",
+                latitude = "22.99785373999657",
+                longitude = "120.19925784172356"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #13
+        elif user_message ==f"可由此打開{food13}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food13}",
+                address = "台南市中西區府前路一段215號",
+                latitude = "22.989241763445442",
+                longitude = "120.20376114791513"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #14
+        elif user_message ==f"可由此打開{food14}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food14}",
+                address = "台南市中西區大德街38號",
+                latitude = "22.989617989057823",
+                longitude = "120.19568503987466"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #15
+        elif user_message ==f"可由此打開{food15}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food15}",
+                address = "台南市中西區民族路三段104號",
+                latitude = "22.99888561400458",
+                longitude = "120.19721339754601"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #16
+        elif user_message ==f"可由此打開{food16}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food16}",
+                address = "台南市北區成功路380號",
+                latitude = "23.000357785827312",
+                longitude = "120.20017912913502"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #17
+        elif user_message ==f"可由此打開{food17}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food17}",
+                address = "台南市中西區國華街三段22號",
+                latitude = "22.993605613377643",
+                longitude = "120.19752086686162"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #18
+        elif user_message ==f"可由此打開{food18}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food18}",
+                address = "台南市中西區保安路72號",
+                latitude = "22.990696347559567",
+                longitude = "120.19620502823015"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #19
+        elif user_message ==f"可由此打開{food19}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food19}",
+                address = "台南市中西區中正路康樂市場180號",
+                latitude = "22.992518455938086",
+                longitude = "120.19615985521696"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
+
+        #20
+        elif user_message ==f"可由此打開{food20}的google地圖位置":
+            output_message = LocationSendMessage(
+                title = f"{food20}",
+                address = "台南市中西區西門路一段728號",
+                latitude = "22.990158601829926",
+                longitude = "120.19791038220367"
+            )
+            line_bot_api.reply_message(event.reply_token, output_message)
 
         #----------------------------------------------------------------------
 

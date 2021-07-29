6390a0ec4ce18e9fa7be13875736d0f2fea9f4b8
Jim Lee <jim880705@gmail.com>
2021-07-29 16:01:19 +0800

    ok

--- start.py
+++ start.py
@@ -1328,15 +1328,15 @@ def handle_message(event):
 
 #------------------------------------------------------------------------------
 
-        # #咖啡廳
-        # elif user_message == "台南咖啡廳":
-        #     reply_arr = []
+        #咖啡廳
+        elif user_message == "台南咖啡廳":
+            reply_arr = []
 
-        #     output_message1 = text_reply.text_reply_message("點選圖片即可獲得資訊！")
-        #     output_message2 = cafe_image_carousel()
+            output_message1 = text_reply.text_reply_message("點選圖片即可獲得資訊！")
+            output_message2 = cafe_image_carousel()
 
-        #     reply_arr.extend([output_message1,output_message2])
-        #     line_bot_api.reply_message(event.reply_token, reply_arr)
+            reply_arr.extend([output_message1,output_message2])
+            line_bot_api.reply_message(event.reply_token, reply_arr)
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
             

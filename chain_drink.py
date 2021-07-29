43540d662e0f70b4bbd8466339cc376174b61821
Jim Lee <jim880705@gmail.com>
2021-07-29 14:34:22 +0800

    ok

--- chain_drink.py
+++ chain_drink.py
@@ -1,35 +1,4 @@
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-from PIL import Image
-from io import StringIO
-
-import requests
-import random
-import json
-import math
-import time
-import datetime
-
-#---------------- self define module ----------------
-import text_push as text_push
-import text_reply as text_reply
-
-#---------------- self define variables ----------------
-from mykey import *
-
-#---------------- line settings ----------------
-# Channel Access Token
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-# Channel Secret
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+from start import *
 
 #---------------------------------------------------
 

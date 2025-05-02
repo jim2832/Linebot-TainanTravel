d8411492be0d1609b4444667a51d94f750df41d8
Jim <jim880705@gmail.com>
2025-05-02 23:54:58 +0800

    250502 update start.py

--- start.py
+++ start.py
@@ -118,8 +118,8 @@ def handle_message(event):
 
         elif user_message == "肥宅快樂水":
             output_message = ImageSendMessage(
-                original_content_url = "https://f.share.photo.xuite.net/chungming01/1fe45d1/10789161/501035944_m.jpg",
-                preview_image_url = "https://f.share.photo.xuite.net/chungming01/1fe45d1/10789161/501035944_m.jpg"
+                original_content_url = "https://etaiwan.blog/wp-content/uploads/20230712074403_19.jpg",
+                preview_image_url = "https://etaiwan.blog/wp-content/uploads/20230712074403_19.jpg"
             )
             line_bot_api.reply_message(event.reply_token, output_message)
 
